082604e1ea8f4b524fb0c797c5ee5cf305d1a524
Pochat2077 <pryadkadima@gmail.com>
2024-09-07 12:34:41 +0300

    lesson1
    1st script and node map

---
+++ TowerDefenseGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private const int NODE_GRID_ROW_COUNT = 4;
+    [SerializeField] 
+    private GameObject nodePref;
+    private void Start()
+    {
+        CreateNodes();
+    }
+    private void CreateNodes()
+    {
+        for(int x = 0; x < NODE_GRID_ROW_COUNT; x++)
+        {
+            for(int z = 0; z < NODE_GRID_ROW_COUNT; z++)
+          {
+            Instantiate(nodePref, new Vector3(x, 0, z), Quaternion.identity);
+          }
+        }
+    }
+
+   
+}
